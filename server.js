234df94d34267676c129cef6d483e63241b7a639
ayush-shrestha1 <np03cs4s210015@heraldcollege.edu.np>
2022-04-29 19:56:48 +0545

    added CRUD

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const res = require("express/lib/response");
 
 // server config
 const server = express();
@@ -14,90 +13,169 @@ mongoose.connect(DB_Connection, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log("Database connected!");
+    console.log("Mongodb Database Connected!");
 }).catch(err => {
     console.log(err);
 });
 
-// data structure
-const userDetails = new mongoose.Schema({
-    email: String,
-    createdOn: String
-})
 
-// defining a model for user
-const userModel = mongoose.model('details', userDetails);
-
-// middleware
+// middleware 
 server.use(express.json());
+
 
 // default routing
 server.get("/", (req, res) => {
-    res.send("Server Started on default route");
-})
+    console.log("Server Started");
+    res.send("Server Started");
+});
 
-// generate token
-const GenerateToken = (email) => {
-    const token = jwt.sign({ id: email }, "secret_key");
+
+// verifying jwt token
+const VerifyJWT = (token, key) => {
+    try {
+        const res = jwt.verify(token, RMS);
+        console.log("Verification Successfull!")
+    } catch (err) {
+        console.log("Verification Failed!");
+    }
+}
+
+
+// generating jwt token
+const GenerateJWT = (uid) => {
+    const token = jwt.sign({ id: uid }, process.env.TOP_SECRET_KEY);
     return token;
 }
 
-//verify
-// jwt.verify(token,key);
 
-// login routing
-server.post("/login", (req, res) => {
-    const { email } = req.body;
-
-    // data mapping
-    userModel.find({
-        email: email
-    }).then((data) => {
-        if (data.length != 0) {
-            try {
-                return res.status(200).send({
-                    message: "Logged in!",
-                    token: GenerateToken(email)
-                });
-            } catch (error) {
-                console.log(error);
-            }
-        }
-    }).catch(err => {
-        return res.status(500).send({
-            message: "INTERNAL SERVER ERROR 500",
-            token: null
-        });
-    })
-
-    // check if user is new
-    registerUser(res, email)
-})
-
-// Register new user
-const registerUser = (res, email) => {
-    const data = new userModel({
-        email: email,
+// register new user
+const registerNewUser = async (res, uid) => {
+    // upload data to database
+    const data = new studentModel({
+        uid: uid,
         createdOn: new Date().toLocaleDateString()
     })
 
-    // save details to MongoDB
-    data.save().then(() => {
-        return resizeBy.status(200).json({
-            message: "Registration Successful!",
-            token: GenerateToken(email)
+    try {
+        // uploading the data to database 
+        const response = await data.save();
+
+        // sending response to the sender
+        res.status(200).json({
+            message: "Registration succesfull !",
+            token: GenerateJWT(uid)
         });
-    }).catch(err => {
-        return res.status(500).send({
-            message: "Registration Failed!",
+    } catch (error) {
+        res.status(500).json({
+            message: "Registration failed !",
             token: null
         });
-    });
+    }
+
 }
 
+// login routing
+server.post("/login", async (req, res) => {
+    // destructuring the incoming data 
+    const { uid } = req.body;
 
+    // database data mapping
+    try {
+        const data = await studentModel.find({ uid: uid });
+        if (data.length != 0) {
+            res.status(200).send({
+                message: "Login succesfull !",
+                token: GenerateJWT(uid)
+            });
+            return;
+        }
+    } catch (error) {
+        // if issue found on server
+        res.status(500).send({
+            message: "500 Internal Server Error !",
+            token: null
+        });
+        return;
+    }
+
+    // if user not found in database
+    registerNewUser(res, uid);
+
+});
+
+
+
+// CRUD Routine Operation
+// post routine data
+server.post("/postRoutineData", (req, res) => {
+    //destructuring incoming data
+    const { module_name, lecturer_name, group, room_name, block_name, timing } = req.body;
+
+    const data = new routineModel({
+        module_name: module_name,
+        lecturer_name: lecturer_name,
+        group: group,
+        room_name: room_name,
+        block_name: block_name,
+        timing: timing
+    });
+
+    data.save().then(() => {
+        res.status(200).send("Routine Posted successfully !");
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+});
+
+
+// get routine data
+server.get("/getRoutineData", (req, res) => {
+    // getting data collection from routine db
+    routineModel.find().then((data) => {
+        res.status(200).send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: "500 Internal Server Error !"
+        });
+    });
+
+});
+
+// update routine data
+server.post("/updateRoutineData", (req, res) => {
+    // get the routine doc id
+    const { routineID, module_name } = req.body;
+    routineModel.findByIdAndUpdate(routineID, {
+        module_name: module_name
+    }, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: "500 Internal Server Error !"
+            });
+        } else {
+            res.status(200).send({
+                message: "Routine Succesfully updated !!"
+            });
+        }
+    })
+});
+
+// delete routine data
+server.post("/deleteRoutineData", (req, res) => {
+    // get the routine doc id
+    const { routineID } = req.body;
+    routineModel.remove({ _id: routineID }).then((data) => {
+        res.status(200).send({
+            message: "Routine Deleted Succesfully !"
+        });
+    }).catch(err => {
+        res.status(500).send({
+            message: "500 Internal Server Error !"
+        });
+    });
+});
 
 // port listner
 server.listen(PORT, () => {
-    console.log("Server Connected");
-})
+    console.log(`Server Connected to the port ${PORT}`);
+});
